1494f39b4a171241e155fff75b8d36910a467fef
David Wursteisen <david.wursteisen+git@gmail.com>
2023-03-24 23:28:52 +0100

    Introduce map methods to draw map on screen

--- src/main/kotlin/com/github/minigdx/tiny/lua/TinyLib.kt
+++ src/main/kotlin/com/github/minigdx/tiny/lua/TinyLib.kt
@@ -38,6 +38,14 @@ annotation class DocArg(
     val documentation: String = "",
 )
 
+@Target(VALUE_PARAMETER, FUNCTION)
+@Retention(SOURCE)
+@Repeatable
+annotation class DocArgs(
+    val names: Array<String>,
+    val documentations: Array<String> = [],
+)
+
 @Target(FUNCTION)
 @Retention(SOURCE)
 annotation class DocCall(
@@ -56,6 +64,10 @@ class MapLib(private val parent: GameScript) : TwoArgFunction() {
 
     inner class draw() : LibFunction() {
 
+        @DocCall(
+            documentation = "Draw the default layer on the screen.",
+            mainCall = true,
+        )
         override fun call(): LuaValue {
             val layer = parent.level?.imageLayers?.get(0)
             if (layer != null) {
@@ -72,7 +84,82 @@ class MapLib(private val parent: GameScript) : TwoArgFunction() {
             return NONE
         }
 
-        override fun call(a: LuaValue): LuaValue {
+        @DocCall(
+            documentation = "Draw the default layer on the screen at the x/y coordinates.",
+        )
+        override fun call(@DocArg("x") a: LuaValue, @DocArg("y") b: LuaValue): LuaValue {
+            val layer = parent.level?.imageLayers?.get(0)
+            if (layer != null) {
+                parent.frameBuffer.colorIndexBuffer.copyFrom(
+                    source = layer.pixels,
+                    dstX = a.checkint(),
+                    dstY = b.checkint(),
+                    sourceX = 0,
+                    sourceY = 0,
+                    width = layer.width,
+                    height = layer.height
+                )
+            }
+            return NONE
+        }
+
+        @DocCall(
+            documentation = "Draw the default layer on the screen at the x/y coordinates.",
+        )
+        override fun call(
+            @DocArg("x") a: LuaValue, @DocArg("y") b: LuaValue,
+            @DocArg("sx") c: LuaValue, @DocArg("sy") d: LuaValue
+        ): LuaValue {
+            val layer = parent.level?.imageLayers?.get(0)
+            if (layer != null) {
+                parent.frameBuffer.colorIndexBuffer.copyFrom(
+                    source = layer.pixels,
+                    dstX = a.checkint(),
+                    dstY = b.checkint(),
+                    sourceX = c.checkint(),
+                    sourceY = d.checkint(),
+                    width = layer.width,
+                    height = layer.height
+                )
+            }
+            return NONE
+        }
+
+        override fun invoke(
+            @DocArgs(
+                names = ["x", "y", "sx", "sy", "width", "height"]
+            ) args: Varargs
+        ): Varargs {
+            if (args.narg() < 6) return super.invoke(args)
+            val x = args.arg(1).checkint()
+            val y = args.arg(2).checkint()
+            val sx = args.arg(3).checkint()
+            val sy = args.arg(4).checkint()
+            val width = args.arg(5).checkint()
+            val height = args.arg(6).checkint()
+
+            val layer = parent.level?.imageLayers?.get(0)
+            if (layer != null) {
+                parent.frameBuffer.colorIndexBuffer.copyFrom(
+                    source = layer.pixels,
+                    dstX = x,
+                    dstY = y,
+                    sourceX = sx,
+                    sourceY = sy,
+                    width = width,
+                    height = height
+                )
+            }
+            return NONE
+        }
+
+        @DocCall(
+            documentation = "Draw the layer on the screen.",
+            mainCall = true
+        )
+        override fun call(
+            @DocArg("layer", "index of the layer") a: LuaValue
+        ): LuaValue {
             val layer = parent.level?.imageLayers?.getOrNull(a.checkint())
             if (layer != null) {
                 parent.frameBuffer.colorIndexBuffer.copyFrom(
--- src/main/resources/boot.lua
+++ src/main/resources/boot.lua
@@ -7,7 +7,7 @@ function draw()
     dt = dt + 1/60
     cls(2)
     table.sort({1, 2, 3})
-     map.draw(2)
+     map.draw(5, 5, 64 + cos(dt) * 64, 8, 32, 32)
     -- toto.draw()
     -- debug.traceback(cls(1))
     spr(4, 64 + cos(dt) * 64, 0, false, cos(dt) > 0)
