b6845a9d76302cb3c77128f824b804e5b3e1f18d
dwursteisen <dwursteisen@users.noreply.github.com>
2023-04-23 12:37:51 +0000

    deploy: e870fec3044e7f674d737f7020c9a0802f5f55c2

--- _boot.lua
+++ _boot.lua
@@ -1,35 +1,61 @@
 function _init()
     ready = false
     dt = 0
-    txt = {"l", "o", "a", "d", "i", "n", "g"}
+
+    prefix = {"b", "u", "i", "l", "d"," ", "w", "i", "t", "h", " "}
+    txt = { "t", "i", "n", "y" }
     waiting = 3
 end
 
 function _update()
-    dt = dt + 1/60
+    dt = dt + 1 / 60
     if ready then
-        waiting = waiting - 1/60
+        waiting = waiting - 1 / 60
     end
 
-    if(waiting < 0) then
+    if (waiting < 0) then
         cls(1)
         exit(0) -- start the first script in the game script stack
     end
 end
 
+function letter(x, l, index, move)
+    local offset = 3
+    if move then
+        offset = abs(cos(dt * PI * 2 + index) * 4)
+    end
+    if ((not ready) and (l ~= " ")) then
+        shape.rectf(
+                x + index * 4 + 1,
+                1 + 8 + offset,
+                4, 4,
+                "#5f574f"
+        )
+        shape.rectf(
+                x + index * 4,
+                8 + offset,
+                4, 4,
+                "#FFFFFF"
+        )
+    else
+        print(l, x + index * 4 + 1, 1 + 8 + offset, "#5f574f")
+        print(l, x + index * 4, 8 + offset, "#FFFFFF")
+    end
+end
 function _draw()
     cls(1)
-    for index=1,#txt do
 
-        print(txt[index], 2 + index * 4 + 1, 1 + 8 + abs(cos(dt * PI * 2 + index) * 4), "#5f574f")
-        print(txt[index], 2 + index * 4, 8 + abs(cos(dt * PI * 2 + index) * 4), "#FFFFFF")
+    for index = 1, #prefix do
+        letter(2, prefix[index], index, false)
     end
-    
+
+    for index = 1, #txt do
+        letter(46, txt[index], index, true)
+    end
+
 
 end
 
 function _resources()
-    -- current = loaded
-    debug("loaded")
     ready = true
-end 
+end
