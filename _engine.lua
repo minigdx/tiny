dec4a61753bce87f91844b3ba749a47e5c7af397
dwursteisen <dwursteisen@users.noreply.github.com>
2023-05-08 19:43:43 +0000

    deploy: 294c1f3c81a5dab181b9e02e95c7946e7c41141a

--- _engine.lua
+++ _engine.lua
@@ -1,22 +1,29 @@
 function _init(w, h)
     dt = 0
-    msg = {logo=0, text="", color=0}
+    msg = { logo = 0, text = "", color = 0 }
     width = w
+    forever = false
 end
 
-function popup(logo, text, color)
-    msg = {logo=logo, text=text, color=color}
+function popup(logo, text, color, keep)
+    msg = { logo = logo, text = text, color = color }
     dt = 2
+    forever = keep
+end
+
+function clear()
+    dt = 0
+    forever = false
 end
 
 function _update()
     if dt > 0 then
-        dt = dt - 1/60
+        dt = dt - 1 / 60
     end
 end
 
 function _draw()
-    if dt > 0 then
+    if forever or dt > 0 then
         shape.rectf(0, 0, width, 6, msg.color)
         -- TODO: display the logo
         print(msg.text, 6, 1, "#FFFFFF")
