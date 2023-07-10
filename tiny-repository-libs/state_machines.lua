7a2ee1f7b836f1f20bb9d6db2f5abbabc272f79d
David Wursteisen <david.wursteisen+git@gmail.com>
2023-07-10 10:51:35 +0200

    Update state_machines.lua to match LUA convention.

--- tiny-repository-libs/state_machines.lua
+++ tiny-repository-libs/state_machines.lua
@@ -11,10 +11,10 @@
     When the key space is pressed in any of those state,
     it switch to another state.
 
-state_machine = require("statemachines")
+state_machine = require("state_machines")
 
 function _init()
-    state_machine:addState("square", {
+    state_machine:add_state("square", {
         _draw = function()
             shape.rectf(128, 128, 64, 64, 1)
         end,
@@ -26,7 +26,7 @@ function _init()
         end
     })
 
-    state_machine:addState("circle", {
+    state_machine:add_state("circle", {
         _draw = function()
             shape.circlef(128, 128, 64, 1)
         end,
@@ -38,7 +38,7 @@ function _init()
         end
     })
 
-    state_machine:init("square")
+    state_machine:set_state("square")
 end
 
 
@@ -55,10 +55,14 @@ end
 ]]--
 local State = {
     name = "", -- name of the state
-    onEnter = function() end, -- method called when the state is entered
-    onLeave = function() end, -- method called when the state is leaved
-    _update = function() end, -- method called on each update
-    _draw = function() end -- method called on each draw
+    on_enter = function()
+    end, -- method called when the state is entered
+    on_leave = function()
+    end, -- method called when the state is leaved
+    _update = function()
+    end, -- method called on each update
+    _draw = function()
+    end -- method called on each draw
 }
 
 local StateMachine = {
@@ -67,13 +71,17 @@ local StateMachine = {
 }
 
 -- Add a new state to the current state machine
-function StateMachine:addState(name, options)
+function StateMachine:add_state(name, options)
     local state = new(State, {
         name = name,
-        onEnter = options.onEnter or function() end,
-        onLeave = options.onLeave or function() end,
-        _update = options._update or function() end,
-        _draw = options._draw or function() end,
+        on_enter = options.on_enter or function()
+        end,
+        on_leave = options.on_leave or function()
+        end,
+        _update = options._update or function()
+        end,
+        _draw = options._draw or function()
+        end,
     })
 
     self.states[name] = state
@@ -81,21 +89,27 @@ function StateMachine:addState(name, options)
     return state
 end
 
--- Set the initial state of the state machine
-function StateMachine:init(name)
-    if selt.current == nil then
+-- Set the current state of the state machine.
+-- If there is a previous state, will leave it.
+function StateMachine:set_state(name)
+    if self.current ~= nil then
+        local current_state = self.states[self.current]
+        current_state:on_leave()
+    end
+    local state = self.states[name]
+    if state ~= nil then
         self.current = name
+        self.states[self.current]:on_enter()
+    else
+        error(name.." state was not configured before. Did you forgot to call add_state?")
     end
 end
 
 function StateMachine:_update()
     if self.current then
-        local currentState = self.states[self.current]
-        local newState = self.states[self.current]:_update()
-        if newState then
-            currentState:onLeave()
-            self.current = newState
-            currentState = self.states[self.current]:onEnter()
+        local new_state = self.states[self.current]:_update()
+        if new_state ~= nil then
+            self:set_state(new_state)
         end
     end
 end
