c34b77590a46af6f6b9636349459946db18ec797
David Wursteisen <david.wursteisen+git@gmail.com>
2023-12-29 21:11:58 +0100

    update vec2 documentation

--- tiny-engine/src/commonMain/kotlin/com/github/minigdx/tiny/lua/Vec2Lib.kt
+++ tiny-engine/src/commonMain/kotlin/com/github/minigdx/tiny/lua/Vec2Lib.kt
@@ -1,5 +1,7 @@
 package com.github.minigdx.tiny.lua
 
+import com.github.mingdx.tiny.doc.TinyArg
+import com.github.mingdx.tiny.doc.TinyCall
 import com.github.mingdx.tiny.doc.TinyFunction
 import com.github.mingdx.tiny.doc.TinyLib
 import org.luaj.vm2.LuaTable
@@ -25,14 +27,17 @@ class Vec2Lib : TwoArgFunction() {
         return vector2
     }
 
-    @TinyFunction("Create a vector 2 as a table { x, y }")
+    @TinyFunction("Create a vector 2 as a table { x, y }.")
     class create : TwoArgFunction() {
-        override fun call(arg1: LuaValue, arg2: LuaValue): LuaValue {
+
+        @TinyCall("Create a vector 2 as a table { x, y }.")
+        override fun call(@TinyArg("x") arg1: LuaValue, @TinyArg("y") arg2: LuaValue): LuaValue {
             val (x, y) = extract(arg1, arg2)
             return v2(x, y)
         }
 
-        override fun call(arg: LuaValue): LuaValue = super.call(arg)
+        @TinyCall("Create a vector 2 as a table { x, y } using another vector 2.")
+        override fun call(@TinyArg("vec2") arg: LuaValue): LuaValue = super.call(arg)
     }
 
     @TinyFunction("Add vector2 to another vector2", example = VECTOR2_ADD)
@@ -44,6 +49,20 @@ class Vec2Lib : TwoArgFunction() {
                 valueOf(y1.todouble() + y2.todouble()),
             )
         }
+
+        @TinyCall("Add a vector 2 {x, y} to another vector 2 {x, y}")
+        override fun call(
+            @TinyArg("v1", "vector 2 as a table {x, y}") a: LuaValue,
+            @TinyArg("v2", "vector 2 as a table {x, y}") b: LuaValue,
+        ): LuaValue = super.call(a, b)
+
+        @TinyCall("Add a destructured vector 2 to another destructured vector 2")
+        override fun call(
+            @TinyArg("x1") a: LuaValue,
+            @TinyArg("y1") b: LuaValue,
+            @TinyArg("x2") c: LuaValue,
+            @TinyArg("y2") d: LuaValue,
+        ): LuaValue = super.call(a, b, c, d)
     }
 
     @TinyFunction("Subtract another vector to a vector", example = VECTOR2_SUB)
@@ -55,14 +74,42 @@ class Vec2Lib : TwoArgFunction() {
                 valueOf(y1.todouble() - y2.todouble()),
             )
         }
+
+        @TinyCall("Subtract a vector 2 {x, y} from another vector 2 {x, y}")
+        override fun call(
+            @TinyArg("v1", "vector 2 as a table {x, y}") a: LuaValue,
+            @TinyArg("v2", "vector 2 as a table {x, y}") b: LuaValue,
+        ): LuaValue = super.call(a, b)
+
+        @TinyCall("Subtract a destructured vector 2 from another destructured vector 2")
+        override fun call(
+            @TinyArg("x1") a: LuaValue,
+            @TinyArg("y1") b: LuaValue,
+            @TinyArg("x2") c: LuaValue,
+            @TinyArg("y2") d: LuaValue,
+        ): LuaValue = super.call(a, b, c, d)
     }
 
-    @TinyFunction("Dot product between two vectors")
+    @TinyFunction("Dot product between two vectors", example = VECTOR_DOT)
     class dot : LibFunction() {
         override fun invoke(args: Varargs): Varargs {
             val (x1, y1, x2, y2) = extract(args)
             return valueOf(x1.todouble() * x2.todouble() + y1.todouble() * y2.todouble())
         }
+
+        @TinyCall("Dot product between a vector 2 {x, y} and another vector 2 {x, y}")
+        override fun call(
+            @TinyArg("v1", "vector 2 as a table {x, y}") a: LuaValue,
+            @TinyArg("v2", "vector 2 as a table {x, y}") b: LuaValue,
+        ): LuaValue = super.call(a, b)
+
+        @TinyCall("Dot product between a destructured vector 2 and another destructured vector 2")
+        override fun call(
+            @TinyArg("x1") a: LuaValue,
+            @TinyArg("y1") b: LuaValue,
+            @TinyArg("x2") c: LuaValue,
+            @TinyArg("y2") d: LuaValue,
+        ): LuaValue = super.call(a, b, c, d)
     }
 
     @TinyFunction("Calculate the magnitude (length) of a vector")
@@ -71,19 +118,39 @@ class Vec2Lib : TwoArgFunction() {
             val (x1, y1) = extract(args)
             return valueOf(kotlin.math.sqrt(x1.todouble() * x1.todouble() + y1.todouble() * y1.todouble()))
         }
+
+        @TinyCall("Calculate the magnitude (length) of a vector 2 {x, y}")
+        override fun call(
+            @TinyArg("x") a: LuaValue,
+            @TinyArg("y") b: LuaValue,
+        ): LuaValue = super.call(a, b)
+
+        @TinyCall("Calculate the magnitude (length) of a vector 2 {x, y}")
+        override fun call(
+            @TinyArg("v1", "vector 2 as a table {x, y}") a: LuaValue,
+        ): LuaValue = super.call(a)
     }
 
-    @TinyFunction("Normalize a vector")
+    @TinyFunction("Normalize a vector", example = VECTOR_NOR)
     class nor : LibFunction() {
         override fun invoke(args: Varargs): Varargs {
             val (x1, y1) = extract(args)
             val len = kotlin.math.sqrt(x1.todouble() * x1.todouble() + y1.todouble() * y1.todouble())
-            if (len != 0.0) {
-                return v2(valueOf(x1.todouble() / len), valueOf(y1.todouble() / len))
+            return if (len != 0.0) {
+                v2(valueOf(x1.todouble() / len), valueOf(y1.todouble() / len))
             } else {
-                return NIL
+                NIL
             }
         }
+
+        @TinyCall("Normalize a vector 2 {x, y}")
+        override fun call(
+            @TinyArg("x") a: LuaValue,
+            @TinyArg("y") b: LuaValue,
+        ): LuaValue = super.call(a, b)
+
+        @TinyCall("Normalize a vector 2 {x, y}")
+        override fun call(@TinyArg("v1", "vector 2 as a table {x, y}") a: LuaValue): LuaValue = super.call(a)
     }
 
     @TinyFunction("Cross product")
@@ -94,6 +161,20 @@ class Vec2Lib : TwoArgFunction() {
                 x1.todouble() * y2.todouble() - y1.todouble() * x2.todouble(),
             )
         }
+
+        @TinyCall("Cross product between a vector 2 {x, y} and another vector 2 {x, y}")
+        override fun call(
+            @TinyArg("v1", "vector 2 as a table {x, y}") a: LuaValue,
+            @TinyArg("v2", "vector 2 as a table {x, y}") b: LuaValue,
+        ): LuaValue = super.call(a, b)
+
+        @TinyCall("Cross product between a destructured vector 2 and another destructured vector 2")
+        override fun call(
+            @TinyArg("x1") a: LuaValue,
+            @TinyArg("y1") b: LuaValue,
+            @TinyArg("x2") c: LuaValue,
+            @TinyArg("y2") d: LuaValue,
+        ): LuaValue = super.call(a, b, c, d)
     }
 
     @TinyFunction("Scale a vector", example = VECTOR2_SCL)
@@ -102,6 +183,19 @@ class Vec2Lib : TwoArgFunction() {
             val (x1, y1, scale) = extract(args)
             return v2(valueOf(x1.todouble() * scale.todouble()), valueOf(y1.todouble() * scale.todouble()))
         }
+
+        @TinyCall("Scale a vector 2 {x, y} using the factor scl")
+        override fun call(
+            @TinyArg("x") a: LuaValue,
+            @TinyArg("y") b: LuaValue,
+            @TinyArg("scl") c: LuaValue,
+        ): LuaValue = super.call(a, b, c)
+
+        @TinyCall("Scale a vector 2 {x, y} using the factor scl")
+        override fun call(
+            @TinyArg("v1", "vector 2 as a table {x, y}") a: LuaValue,
+            @TinyArg("scl") b: LuaValue,
+        ): LuaValue = super.call(a, b)
     }
 
     companion object {
--- tiny-engine/src/commonMain/kotlin/com/github/minigdx/tiny/lua/Vec2LibExamples.kt
+++ tiny-engine/src/commonMain/kotlin/com/github/minigdx/tiny/lua/Vec2LibExamples.kt
@@ -63,3 +63,52 @@ function _init()
     shape.line(0, 0, v1.x, v1.y, 10)
 end
 """
+
+//language=Lua
+const val VECTOR_DOT = """
+function _init()
+    angle = 0
+end
+function _update()
+
+    if ctrl.pressing(keys.space) then
+        angle = angle + 0.1
+    end
+
+    gfx.camera(-128, -128)
+
+    local v1 = vec2.create(1, 0)
+    local v2 = vec2.create(math.cos(angle), math.sin(angle))
+    local dot = vec2.dot(v1, v2)
+    
+    local scl = vec2.scl(v1, dot)
+    
+    gfx.cls()
+    
+    local scaledv1 = vec2.scl(v1, 64)
+    local scaledv2 = vec2.scl(v2, 64)
+    local scaledv3 = vec2.scl(scl, 64)
+
+    print("v1", 18, 5, 10)
+    shape.line(0, 0, scaledv1.x, scaledv1.y, 10)
+    print("v2", scaledv2.x + 5, scaledv2.y + 5, 11)
+    shape.line(0, 0, scaledv2.x, scaledv2.y, 11)
+    
+    print("dot", scaledv3.x, 5, 9)
+    shape.line(0, 0, scaledv3.x, scaledv3.y, 9)
+end    
+"""
+
+//language=Lua
+const val VECTOR_NOR = """
+function _update()
+    gfx.cls()
+
+    local v0 = vec2.create(43, 64)
+    local v1 = vec2.nor(v0)
+    
+    
+    print("vector     x: " .. v0.x .. " y: " .. v0.y)
+    print("normalized x: " .. v1.x .. " y: " .. v1.y, 0, 8)
+end 
+"""
