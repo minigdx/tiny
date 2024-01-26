61e5d425d466db70aa3bb8d8cd067da20ed6d17e
David Wursteisen <david.wursteisen+git@gmail.com>
2024-01-26 13:49:48 +0100

    play sfx also on the web platform

--- tiny-engine/src/jsMain/kotlin/com/github/minigdx/tiny/platform/webgl/PicoAudioSoundMananger.kt
+++ tiny-engine/src/jsMain/kotlin/com/github/minigdx/tiny/platform/webgl/PicoAudioSoundMananger.kt
@@ -2,6 +2,7 @@ package com.github.minigdx.tiny.platform.webgl
 
 import com.github.minigdx.tiny.Seconds
 import com.github.minigdx.tiny.input.InputHandler
+import com.github.minigdx.tiny.lua.SfxLib
 import com.github.minigdx.tiny.sound.Sound
 import com.github.minigdx.tiny.sound.SoundManager
 import com.github.minigdx.tiny.sound.SoundManager.Companion.SAMPLE_RATE
@@ -27,12 +28,41 @@ class PicoAudioSound(val audio: dynamic, val smf: dynamic) : Sound {
     }
 }
 
+class SfxSound(
+    private val buffer: Float32Array,
+    private val picoAudioSoundMananger: PicoAudioSoundMananger,
+) : Sound {
+
+    private var currentSource: AudioBufferSourceNode? = null
+
+    override fun play() {
+        stop()
+        currentSource = picoAudioSoundMananger.playSfxBuffer(buffer)
+    }
+
+    override fun loop() {
+        currentSource = picoAudioSoundMananger.playSfxBuffer(buffer, loop = true)
+    }
+
+    override fun stop() {
+        currentSource?.stop()
+    }
+}
+
 class PicoAudioSoundMananger : SoundManager {
 
     lateinit var audioContext: AudioContext
 
     override fun initSoundManager(inputHandler: InputHandler) {
         audioContext = AudioContext()
+    }
+
+    override suspend fun createSfxSound(bytes: ByteArray): Sound {
+        val score = bytes.decodeToString()
+        val duration = 60f / 120f / 4.0f
+        val waves = SfxLib.convertScoreToWaves(score, duration)
+        val buffer = generateSfxBuffer((waves.size * duration * SAMPLE_RATE).toInt(), waves)
+        return SfxSound(buffer, this)
     }
 
     override suspend fun createMidiSound(data: ByteArray): Sound {
@@ -65,28 +95,43 @@ class PicoAudioSoundMananger : SoundManager {
         if (notes.isEmpty()) return
 
         val numSamples: Int = (SAMPLE_RATE * notes.first().duration * notes.size).toInt()
+
+        val result = generateSfxBuffer(numSamples, notes)
+
+        playSfxBuffer(result)
+    }
+
+    internal fun playSfxBuffer(result: Float32Array, loop: Boolean = false): AudioBufferSourceNode {
         val sfxBuffer = audioContext.createBuffer(
             1,
-            numSamples,
+            result.length,
             SAMPLE_RATE,
         )
 
+        val channel = sfxBuffer.getChannelData(0)
+        channel.set(result)
+
+        val source = audioContext.createBufferSource()
+        source.buffer = sfxBuffer
+        source.connect(audioContext.destination)
+        source.loop = loop
+        source.start()
+        return source
+    }
+
+    internal fun generateSfxBuffer(
+        numSamples: Int,
+        notes: List<WaveGenerator>,
+    ): Float32Array {
         var currentIndex = 0
         val result = Float32Array(numSamples)
-        val channel = sfxBuffer.getChannelData(0)
 
         notes.forEach {
             val buffer = toAudioBuffer(listOf(it), it.duration)
             result.set(buffer.getChannelData(0), currentIndex)
             currentIndex += buffer.getChannelData(0).length
         }
-
-        channel.set(result)
-
-        val source = audioContext.createBufferSource()
-        source.buffer = sfxBuffer
-        source.connect(audioContext.destination)
-        source.start()
+        return result
     }
 
     override fun playNotes(notes: List<WaveGenerator>, longestDuration: Seconds) {
