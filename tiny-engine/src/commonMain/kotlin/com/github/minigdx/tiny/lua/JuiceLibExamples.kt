dc72ea7b49b111520dfc0a72400d86f841bfad3e
David Wursteisen <david.wursteisen+git@gmail.com>
2024-03-19 22:39:38 +0100

    Alow examples to be templated with the function name.

--- tiny-engine/src/commonMain/kotlin/com/github/minigdx/tiny/lua/JuiceLibExamples.kt
+++ tiny-engine/src/commonMain/kotlin/com/github/minigdx/tiny/lua/JuiceLibExamples.kt
@@ -2,44 +2,31 @@ package com.github.minigdx.tiny.lua
 
 //language=Lua
 const val JUICE_EXAMPLE = """
-function _init()
-  t = 0
-end
+local center_x = 256 * 0.5
+local center_y = 256 * 0.5
+local width = 128
 
 function _update()
-  t = t + 1/60
+    gfx.cls()
+    shape.line(center_x - 64, center_y + 64, center_x + 64, center_y + 64, 2)
+    shape.line(center_x + 64, center_y - 64, center_x + 64, center_y + 64, 2)
+
+    
+    for x = 0, width, 2 do
+        local y = juice.##function##(0, 128, x / width)
+        gfx.pset(
+            center_x - 64 + x, center_y + 64 - y, 3
+        )
+    end
+
+    local percent = (tiny.frame % 100) / 100
+    local x = width * percent
+    local y = juice.##function##(0, 128, percent)
+    shape.circlef(center_x - 64 + x, center_y + 64 - y, 4, 7)
+    shape.rectf(center_x - 64 + x - 2, center_y + 64 + 8, 4, 4, 7)
+    shape.rectf(center_x + 70, center_y + 64 - y, 4, 4, 7)
+    local name = "##function##"
+    print(name, center_x - #name * 4 * 0.5, center_y + 92)
 end
 
-function _draw()
-  gfx.cls()
-  progress = math.abs(math.cos(t))
-
-  print("pow", 0, 0) 
-  shape.circlef(juice.pow2(20, 236, progress), 10, 10, 2)
-  shape.circlef(juice.pow3(20, 236, progress), 20, 10, 3)
-  shape.circlef(juice.pow4(20, 236, progress), 30, 10, 4)
-
-
-  print("bounce", 0, 50) 
-  shape.circlef(juice.bounce(20, 236, progress), 60, 10, 5)
-
-  print("exp", 0, 80) 
-  shape.circlef(juice.exp10(20, 236, progress), 90, 10, 6)
-
-
-  print("swing", 0, 110) 
-  shape.circlef(juice.swing(20, 236, progress), 120, 10, 7)
-
-  print("sine", 0, 140) 
-  shape.circlef(juice.sine(20, 236, progress), 150, 10, 8)
-
-  print("circle", 0, 170) 
-  shape.circlef(juice.circle(20, 236, progress), 180, 10, 9)
-
-  print("elastic", 0, 200) 
-  shape.circlef(juice.elastic(20, 236, progress), 210, 10, 10)
-
-  print("linear", 0, 230) 
-  shape.circlef(juice.linear(20, 236, progress), 240, 10, 1)
-end
 """
