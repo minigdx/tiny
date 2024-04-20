05bcf36ede1e30a821efbb97cd84f6aced8e1cf7
David Wursteisen <david.wursteisen+git@gmail.com>
2024-04-20 23:16:39 +0200

    Fix bug on the Vec2 library by having a better management of default values.

---
+++ tiny-engine/src/commonTest/kotlin/com/github/minigdx/tiny/lua/Vec2LibTest.kt
@@ -0,0 +1,43 @@
+package com.github.minigdx.tiny.lua
+
+import org.luaj.vm2.LuaValue.Companion.valueOf
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Vec2LibTest {
+
+    @Test
+    fun it_creates_empty_vec2() {
+        val result = Vec2Lib.create().call()
+        assertEquals(result.get("x"), valueOf(0))
+        assertEquals(result.get("y"), valueOf(0))
+    }
+
+    @Test
+    fun it_creates_an_vec2() {
+        val result = Vec2Lib.create().call(valueOf(3), valueOf(4))
+        assertEquals(result.get("x"), valueOf(3))
+        assertEquals(result.get("y"), valueOf(4))
+    }
+
+    @Test
+    fun it_normalize_a_vector() {
+        val result = Vec2Lib.nor().call(Vec2Lib.create().call(valueOf(2), valueOf(0)))
+        assertEquals(result.get("x"), valueOf(1))
+        assertEquals(result.get("y"), valueOf(0))
+    }
+
+    @Test
+    fun it_normalize_coordinates() {
+        val result = Vec2Lib.nor().call(valueOf(5), valueOf(0))
+        assertEquals(result.get("x"), valueOf(1))
+        assertEquals(result.get("y"), valueOf(0))
+    }
+
+    @Test
+    fun it_normalize_empty_vector() {
+        val result = Vec2Lib.nor().call(valueOf(0), valueOf(0))
+        assertEquals(result.get("x"), valueOf(0))
+        assertEquals(result.get("y"), valueOf(0))
+    }
+}
